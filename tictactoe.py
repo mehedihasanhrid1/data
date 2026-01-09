2b143fb52fcae09b56f71bcf2d52280d1bcb19bc
Mehedi Hasan <mehedihasanhrid1@gmail.com>
2026-01-09 18:12:39 +0600

    Add Tic Tac Toe game with AI and display features
    Implement a Tic Tac Toe game with AI opponent

---
+++ tictactoe.py
@@ -0,0 +1,53 @@
+import random
+from colorama import init, Fore, Style
+
+init(autoreset=True)
+
+def display_board(board):
+    print()
+    def colored(cell):
+        if cell == 'X':
+            return Fore.RED + cell + Style.RESET_ALL
+        elif cell == 'O':
+            return Fore.BLUE + cell + Style.RESET_ALL
+        else:
+            return Fore.YELLOW + cell + Style.RESET_ALL
+    print(' ' + colored(board[0]) + ' | ' + colored(board[1]) + ' | ' + colored(board[2]))
+    print(Fore.CYAN + '-----------' + Style.RESET_ALL)
+    print(' ' + colored(board[3]) + ' | ' + colored(board[4]) + ' | ' + colored(board[5]))
+    print(Fore.CYAN + '-----------' + Style.RESET_ALL)
+    print(' ' + colored(board[6]) + ' | ' + colored(board[7]) + ' | ' + colored(board[8]))
+    print()
+
+def ai_move(board, ai_symbol, player_symbol):
+    for i in range(9):
+        if board[i].isdigit():
+            board_copy = board.copy()
+            board_copy[i] = ai_symbol
+            if check_win(board_copy, ai_symbol):
+                board[i] = ai_symbol
+                return
+    for i in range(9):
+        if board[i].isdigit():
+            board_copy = board.copy()
+            board_copy[i] = player_symbol
+            if check_win(board_copy, player_symbol):
+                board[i] = ai_symbol
+                return
+    possible_moves = [i for i in range(9) if board[i].isdigit()]
+    move = random.choice(possible_moves)
+    board[move] = ai_symbol
+
+def check_win(board, symbol):
+    win_conditions = [
+        (0, 1, 2), (3, 4, 5), (6, 7, 8),    # Horizontal
+        (0, 3, 6), (1, 4, 7), (2, 5, 8),    # Vertical
+        (0, 4, 8), (2, 4, 6)                # Diagonal
+    ]
+    for cond in win_conditions:
+        if board[cond[0]] == board[cond[1]] == board[cond[2]] == symbol:
+            return True
+    return False
+
+def check_full(board):
+    return all(not spot.isdigit() for spot in board)
