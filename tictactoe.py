fb341e59859a69937eda9b24bd259d7269cc0422
Mehedi Hasan <mehedihasanhrid1@gmail.com>
2026-01-09 18:25:19 +0600

    Refactor AI move and board display functions

--- tictactoe.py
+++ tictactoe.py
@@ -4,50 +4,47 @@ from colorama import init, Fore, Style
 init(autoreset=True)
 
 def display_board(board):
+    def c(v):
+        if v == 'X':
+            return Fore.RED + v
+        if v == 'O':
+            return Fore.BLUE + v
+        return Fore.YELLOW + v
+
     print()
-    def colored(cell):
-        if cell == 'X':
-            return Fore.RED + cell + Style.RESET_ALL
-        elif cell == 'O':
-            return Fore.BLUE + cell + Style.RESET_ALL
-        else:
-            return Fore.YELLOW + cell + Style.RESET_ALL
-    print(' ' + colored(board[0]) + ' | ' + colored(board[1]) + ' | ' + colored(board[2]))
-    print(Fore.CYAN + '-----------' + Style.RESET_ALL)
-    print(' ' + colored(board[3]) + ' | ' + colored(board[4]) + ' | ' + colored(board[5]))
-    print(Fore.CYAN + '-----------' + Style.RESET_ALL)
-    print(' ' + colored(board[6]) + ' | ' + colored(board[7]) + ' | ' + colored(board[8]))
+    print(f" {c(board[0])} | {c(board[1])} | {c(board[2])}")
+    print(Fore.CYAN + "-----------")
+    print(f" {c(board[3])} | {c(board[4])} | {c(board[5])}")
+    print(Fore.CYAN + "-----------")
+    print(f" {c(board[6])} | {c(board[7])} | {c(board[8])}")
     print()
 
-def ai_move(board, ai_symbol, player_symbol):
-    for i in range(9):
-        if board[i].isdigit():
-            board_copy = board.copy()
-            board_copy[i] = ai_symbol
-            if check_win(board_copy, ai_symbol):
-                board[i] = ai_symbol
-                return
-    for i in range(9):
-        if board[i].isdigit():
-            board_copy = board.copy()
-            board_copy[i] = player_symbol
-            if check_win(board_copy, player_symbol):
-                board[i] = ai_symbol
-                return
-    possible_moves = [i for i in range(9) if board[i].isdigit()]
-    move = random.choice(possible_moves)
-    board[move] = ai_symbol
-
-def check_win(board, symbol):
-    win_conditions = [
-        (0, 1, 2), (3, 4, 5), (6, 7, 8),    # Horizontal
-        (0, 3, 6), (1, 4, 7), (2, 5, 8),    # Vertical
-        (0, 4, 8), (2, 4, 6)                # Diagonal
+def check_win(board, s):
+    wins = [
+        (0,1,2),(3,4,5),(6,7,8),
+        (0,3,6),(1,4,7),(2,5,8),
+        (0,4,8),(2,4,6)
     ]
-    for cond in win_conditions:
-        if board[cond[0]] == board[cond[1]] == board[cond[2]] == symbol:
-            return True
-    return False
+    return any(board[a] == board[b] == board[c] == s for a,b,c in wins)
 
 def check_full(board):
-    return all(not spot.isdigit() for spot in board)
+    return all(not x.isdigit() for x in board)
+
+def ai_move(board, ai, player):
+    for i in range(9):
+        if board[i].isdigit():
+            temp = board.copy()
+            temp[i] = ai
+            if check_win(temp, ai):
+                board[i] = ai
+                return
+
+    for i in range(9):
+        if board[i].isdigit():
+            temp = board.copy()
+            temp[i] = player
+            if check_win(temp, player):
+                board[i] = ai
+                return
+            
+    board[random.choice([i for i in range(9) if board[i].isdigit()])] = ai
