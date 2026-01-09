848b8139830889c2be2d3cad4b0d721df494c0df
Mehedi Hasan <mehedihasanhrid1@gmail.com>
2026-01-09 18:34:01 +0600

    Refactor Tic Tac Toe functions for clarity

--- tictactoe.py
+++ tictactoe.py
@@ -4,47 +4,51 @@ from colorama import init, Fore, Style
 init(autoreset=True)
 
 def display_board(board):
-    def c(v):
-        if v == 'X':
-            return Fore.RED + v
-        if v == 'O':
-            return Fore.BLUE + v
-        return Fore.YELLOW + v
+    def format_cell(cell_value):
+        if cell_value == 'X':
+            return Fore.RED + cell_value
+        if cell_value == 'O':
+            return Fore.BLUE + cell_value
+        return Fore.YELLOW + cell_value
 
     print()
-    print(f" {c(board[0])} | {c(board[1])} | {c(board[2])}")
+    print(f" {format_cell(board[0])} | {format_cell(board[1])} | {format_cell(board[2])}")
     print(Fore.CYAN + "-----------")
-    print(f" {c(board[3])} | {c(board[4])} | {c(board[5])}")
+    print(f" {format_cell(board[3])} | {format_cell(board[4])} | {format_cell(board[5])}")
     print(Fore.CYAN + "-----------")
-    print(f" {c(board[6])} | {c(board[7])} | {c(board[8])}")
+    print(f" {format_cell(board[6])} | {format_cell(board[7])} | {format_cell(board[8])}")
     print()
 
-def check_win(board, s):
-    wins = [
-        (0,1,2),(3,4,5),(6,7,8),
-        (0,3,6),(1,4,7),(2,5,8),
-        (0,4,8),(2,4,6)
+def check_win(board, player_symbol):
+    winning_positions = [
+        (0, 1, 2), (3, 4, 5), (6, 7, 8),
+        (0, 3, 6), (1, 4, 7), (2, 5, 8),
+        (0, 4, 8), (2, 4, 6)
     ]
-    return any(board[a] == board[b] == board[c] == s for a,b,c in wins)
+    return any(
+        board[a] == board[b] == board[c] == player_symbol
+        for a, b, c in winning_positions
+    )
 
 def check_full(board):
-    return all(not x.isdigit() for x in board)
+    return all(not cell.isdigit() for cell in board)
 
-def ai_move(board, ai, player):
-    for i in range(9):
-        if board[i].isdigit():
-            temp = board.copy()
-            temp[i] = ai
-            if check_win(temp, ai):
-                board[i] = ai
+def ai_move(board, ai_symbol, player_symbol):
+    for index in range(9):
+        if board[index].isdigit():
+            simulated_board = board.copy()
+            simulated_board[index] = ai_symbol
+            if check_win(simulated_board, ai_symbol):
+                board[index] = ai_symbol
                 return
 
-    for i in range(9):
-        if board[i].isdigit():
-            temp = board.copy()
-            temp[i] = player
-            if check_win(temp, player):
-                board[i] = ai
+    for index in range(9):
+        if board[index].isdigit():
+            simulated_board = board.copy()
+            simulated_board[index] = player_symbol
+            if check_win(simulated_board, player_symbol):
+                board[index] = ai_symbol
                 return
-            
-    board[random.choice([i for i in range(9) if board[i].isdigit()])] = ai
+
+    available_positions = [index for index in range(9) if board[index].isdigit()]
+    board[random.choice(available_positions)] = ai_symbol
